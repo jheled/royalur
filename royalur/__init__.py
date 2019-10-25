2dc9fac599c258602381caadb4475f26e433b81e
Joseph Heled <jheled@gmail.com>
2019-10-26 09:06:48 +1300

    version number

--- royalur/__init__.py
+++ royalur/__init__.py
@@ -27,5 +27,5 @@ from .humanStrategies import bestHumanStrategySoFar
 import os.path
 royalURdataDir = os.path.realpath(os.path.join(os.path.dirname(__file__), "data"))
 
-__version__ = "0.2.1.dev1"
+__version__ = "0.2.1.dev3"
 """The version of royalUr"""
